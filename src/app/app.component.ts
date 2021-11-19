3cf765170cd56907ae3c73e5b48a2247d1797619
Douglas C <doogalbeez@outlook.com>
2021-11-18 23:16:59 -0900

    testing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}} <br> {{words}}</br>1</div>`,
 })
 export class AppComponent {
   value = 'World';
+  words = 'This is running on Azure'
 }
